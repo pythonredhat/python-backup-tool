85f5e9c95f48b1b460f6037b95d7c83d7a3e7285
pythonredhat <pythonredhat1987@gmail.com>
2019-11-18 17:13:54 -0500

    Update config

--- python-backup-tool/config.py
+++ python-backup-tool/config.py
@@ -1,4 +1,4 @@
-source_dir =  "/opt/django_developer_portfolio"
+source_dir =  "/opt/django/developer/portfolio"
 destination_dir = "/tmp/backups/"
 destination_ip = "192.168.56.102"
 rsync_user = "root"
