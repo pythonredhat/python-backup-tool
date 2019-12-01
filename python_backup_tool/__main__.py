8d9ad238bcb002ff78feff56f3c8e347097c3600
pythonredhat <pythonredhat1987@gmail.com>
2019-12-01 10:14:26 -0500

    Add full loader

--- python_backup_tool/__main__.py
+++ python_backup_tool/__main__.py
@@ -4,7 +4,7 @@ import yaml
 
 def main():
     with open('/usr/bin/backup/config.yml', 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     process = Rsyncer(cfg['source_dir'], cfg['destination_dir'], cfg['destination_ip'], cfg['rsync_user'])
     process.confirm_dir_exists()
     process.confirm_ssh_connection()
